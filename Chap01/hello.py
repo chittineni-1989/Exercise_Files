6b50e2eb7732a79ecab63b15a1cff7cd02d48752
NAVEEN CHITTINENI <chittinenikrishna@gmail.com>
2022-07-29 07:37:49 +0530

    addded the list file..

--- Chap01/hello.py
+++ Chap01/hello.py
@@ -2,3 +2,32 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 print('Hello, World.')
+print(2)
+
+a= 10
+b= '22.5'
+d= '20'
+
+print("type of a before convert",type(a))
+print("type of a before convert", type(b))
+print("type of a before convert", type(d))
+# b = float(b)
+# print("type of a before convert", type(b))
+# b = int(b)
+
+d = int(d,3)
+print(d)
+c= a + int(float(b))
+print(c)
+print("the type of c is", type(c))
+
+w = None
+print("The type of w is:",type(w))
+w =c
+print(c)
+#
+# ord()
+# hex()
+# oct()
+# eval()
+
